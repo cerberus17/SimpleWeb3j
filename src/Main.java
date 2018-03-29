dd96e9d69c3ba110d43689d44a140f9dfa0740b6
Adam Kipnis <adamkipnis@yahoo.com>
2018-03-29 09:45:40 -0700

    Added GIT ignore and cleaned up code.

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,7 @@ public class Main {
       Event event = new Event("MyEvent",
                               Arrays.asList(new TypeReference<Address>() {}),
                               Arrays.asList(new TypeReference<Uint256>() {}, new TypeReference<Uint256>() {}));
-      
+
       // Note contract address here. See https://github.com/web3j/web3j/issues/405
       EthFilter filter = new EthFilter(DefaultBlockParameterName.EARLIEST, DefaultBlockParameterName.LATEST, "6dd7c1c13df7594c27e0d191fd8cc21efbfc98b4");
 
@@ -51,35 +51,33 @@ public class Main {
     try {
       Web3j web3j = Web3j.build(new HttpService());
 
+      // Static Ganache test account keys
       String rawPrivateKey = "70f1384b24df3d2cdaca7974552ec28f055812ca5e4da7a0ccd0ac0f8a4a9b00";
       String rawPublicKey = "4c9b23b009630f4ba57cb2ed80916938aaacf752ed2379c42bcd000be37c5316bff7afec6c808d66cd2fd69874a8df97eff5";
 
-      BigInteger publicKey = new BigInteger(rawPublicKey, 16);
-      BigInteger privateKey = new BigInteger(rawPrivateKey, 16);
+//      BigInteger publicKey = new BigInteger(rawPublicKey, 16);
+//      BigInteger privateKey = new BigInteger(rawPrivateKey, 16);
 
-      ECKeyPair pair = new ECKeyPair(privateKey, publicKey);
+//      ECKeyPair pair = new ECKeyPair(privateKey, publicKey);
 
-// get current created account
       EthAccounts accounts = web3j.ethAccounts().send();
       String lastAccount = accounts.getAccounts().get(accounts.getAccounts().size() - 1);
-// get creadentials for the first account having some ether
       String firstAccount = web3j.ethAccounts().send().getAccounts().get(0);
       Credentials credentials = Credentials.create(rawPrivateKey);
 
-// get current balance for first account
       EthGetBalance balance = web3j.ethGetBalance(firstAccount, DefaultBlockParameterName.LATEST).send();
-      BigDecimal balanceVaue = Convert.fromWei(balance.getBalance().toString(), Convert.Unit.ETHER);
+      BigDecimal balanceValue = Convert.fromWei(balance.getBalance().toString(), Convert.Unit.ETHER);
 
-// create transaction to give the new created account some ether from the first one
-// log some stuff
       System.out.println("Account: " + firstAccount);
-      System.out.println("Account balance: " + balanceVaue);
-//      System.out.println("Gas Price admin.ethGasPrice() in Ether: " + Convert.fromWei(gasPrice.toString(), Convert.Unit.ETHER));
+      System.out.println("Account balance: " + balanceValue);
       System.out.println("Transfer Gas Limit in Ether: " + Convert.fromWei(Transfer.GAS_LIMIT.toString(), Convert.Unit.ETHER));
       System.out.println("Transfer Gas Price in Ether: " + Convert.fromWei(Transfer.GAS_PRICE.toString(), Convert.Unit.ETHER));
 
       TransactionReceipt transactionReceipt = Transfer.sendFunds(web3j, credentials, lastAccount, BigDecimal.valueOf(10.0), Convert.Unit.ETHER).send();
-      String transactionHash = transactionReceipt.getTransactionHash();
+
+      if (transactionReceipt != null) {
+        System.out.println("Transaction receipt info: " + transactionReceipt.toString());
+      }
     }
     catch (Throwable t) {
       t.printStackTrace();
